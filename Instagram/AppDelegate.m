74aae1164f664d11950f57b939236580a7141c40
Tomisin Ogunfunmi <tomisin@fb.com>
2021-07-06 15:19:27 -0500

    Initial Commit, Set up Backend

--- Instagram/AppDelegate.m
+++ Instagram/AppDelegate.m
@@ -6,6 +6,7 @@
 //
 
 #import "AppDelegate.h"
+#import "Parse/Parse.h"
 
 @interface AppDelegate ()
 
@@ -16,6 +17,27 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    ParseClientConfiguration *config = [ParseClientConfiguration  configurationWithBlock:^(id<ParseMutableClientConfiguration> configuration) {
+
+            configuration.applicationId = @"sFZGMatUWHXRyzebxbyrR5e3ioIaNIX8MKfmI8vE"; // <- UPDATE
+            configuration.clientKey = @"OD8q3NYNyzcEBUJsMVGZTZi8g66NTHmDE1t0hBYi"; // <- UPDATE
+            configuration.server = @"https://parseapi.back4app.com";
+        }];
+
+    [Parse initializeWithConfiguration:config];
+    
+    PFObject *gameScore = [PFObject objectWithClassName:@"GameScore"];
+     gameScore[@"score"] = @1337;
+     gameScore[@"playerName"] = @"Sean Plott";
+     gameScore[@"cheatMode"] = @NO;
+     [gameScore saveInBackgroundWithBlock:^(BOOL succeeded, NSError *error) {
+      if (succeeded) {
+             NSLog(@"Object saved!");
+      } else {
+             NSLog(@"Error: %@", error.description);
+      }
+     }];
+    
     return YES;
 }
 
